f05322f21147eca111c0704a54da0c1c52b61c62
Phobos-Duck <161325120+Phobos-Duck@users.noreply.github.com>
2025-03-15 03:21:34 +0600

    Initial commit

---
+++ src/main/java/webapp/service/VolumeService.java
@@ -0,0 +1,18 @@
+package webapp.service;
+
+public class VolumeService {
+    public double calculateVolume(String shape, double param1, double param2) {
+        if (shape.equals("cube")) {
+            return Math.pow(param1, 3);
+        } else if (shape.equals("sphere")) {
+            return (4.0 / 3.0) * Math.PI * Math.pow(param1, 3);
+        } else if (shape.equals("tetrahedron")) {
+            return (Math.pow(param1, 3) / (6 * Math.sqrt(2)));
+        } else if (shape.equals("torus")) {
+            return (Math.PI * Math.pow(param2, 2)) * (2 * Math.PI * param1);
+        } else if (shape.equals("ellipsoid")) {
+            return (4.0 / 3.0) * Math.PI * param1 * param1 * param2;
+        }
+        return 0;
+    }
+}
