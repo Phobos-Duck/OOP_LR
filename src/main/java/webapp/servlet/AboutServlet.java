0976873ff9f440d7422addd65ff576efaf1be40d
Phobos-Duck <161325120+Phobos-Duck@users.noreply.github.com>
2025-06-04 05:44:19 +0600

    task skeleton

--- src/main/java/webapp/servlet/AboutServlet.java
+++ src/main/java/webapp/servlet/AboutServlet.java
@@ -1,14 +1,18 @@
 package webapp.servlet;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import webapp.service.VolumeService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/about")
 public class AboutServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.getRequestDispatcher("/about.jsp").forward(request, response);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("about.html");
     }
 }
--- src/main/java/webapp/servlet/ContactServlet.java
+++ src/main/java/webapp/servlet/ContactServlet.java
@@ -1,15 +1,19 @@
 package webapp.servlet;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import webapp.service.VolumeService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/contact")
 public class ContactServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.getRequestDispatcher("/contact.jsp").forward(request, response);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("contacts.html");
     }
 }
 
